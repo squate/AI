910afc9817a11de1f079b586108c02cec25d17db
Alan Sato <halfcreative@gmail.com>
2018-02-15 09:45:45 -0500

    started the path record, and trimming the path recordwq

--- hw3/agent.py
+++ hw3/agent.py
@@ -18,13 +18,12 @@ class Agent: #comment
         self.name = "Bruce"
         self.locX = 0
         self.locY = 0
-        self.stepCount = 0
-        #self.square = 0
+        self.stepCount = 0 
         #self.adj = []
         self.queue = []
         self.stack = []
         self.visited = []
-        self.history = []
+        self.pathRecord = []
 #:::::Getters and Setters:::::
     def setName (self,name): #set method for name
         self.name = name
@@ -40,6 +39,8 @@ class Agent: #comment
         return self.stepCount
     def setSquare(self,env):
         self.square = self.locX * env.getSideLength() + self.locY()
+    def nodeToSquare(self,env,x,y):
+        return x * env.getSideLength() + y
     def squareToNode(self,env,sqr):
         square = sqr
         l = env.getSideLength()
@@ -83,6 +84,7 @@ class Agent: #comment
                 if self.squareToNode(env,i) != 1:
                     adjacent.append(i)
         return adjacent
+    #Probably only need expand or getAdjacencies but we can do this later
     def expand(self,env,sqr):
         neighbors = []
         square = sqr
@@ -92,7 +94,9 @@ class Agent: #comment
                     neighbors.append(i)
                 #log(i)
         return neighbors
-#Find Heuristic Value of each square using Distance formula
+
+    #Find Heuristic Value of each square using Distance formula
+    #this will probably be used for A*
     def findHyoo(self,env,x,y):
         goalx = env.getSideLength()-1
         goaly = env.getSideLength()-1
@@ -100,13 +104,26 @@ class Agent: #comment
         d = ((((goalx-x)**2) + ((goaly-y)**2))**(1/2.0))
         #print("distance from {0}{1} to goal is {2}".format(x,y,d))
         return d
-#:::::algorithms:::::
 
+    def trimPath(self,env):
+        temp = []
+        goal = self.pathRecord.pop()
+        while goal[1]!=((env.getSideLength()**2)-1):
+            goal = self.pathRecord.pop()
+        print("Goal is {0}, and we got there from {1}".format(goal[1],goal[0]))
+        temp.append(goal)
+        while temp[0][0] != 0:
+            for item in self.pathRecord:
+                if item[1] == temp[0][0]:
+                    temp.insert(0,item)
+        print("this is the trimmed path{0}".format(temp))
+        return
+#:::::Search Algorithms:::::
     def DFS(self,env):
         #reset stack
         self.stack = []
-        self.visited = [0]
-        self.stack += self.getAdjacencies(env)
+        self.visited = [self.nodeToSquare(env,0,0)]
+        self.stack += self.getAdjacencies(env) #push onto stack the squares that you can move to from origin
         while (len(self.stack) > 0):
             parent = self.stack.pop();
             print("Parent = {0}".format(parent))
@@ -118,27 +135,29 @@ class Agent: #comment
                     continue
                 if item not in self.stack:
                     self.stack.append(item)
-                    if self.squareToNode(env,item) ==1:
-                        print("about to add an item, taken from expand that is a wall")
+                    #if self.squareToNode(env,item) ==1:
+                    #    print("about to add an item, taken from expand that is a wall")
                     print("added Square:{0}: to stack, stack is now {1}".format(item,self.stack))
             self.visited.append(parent) 
     def BFS(self,env):
         #reset queue
-        self.queue = []
-        self.visited = [0]
-        self.queue += self.getAdjacencies(env)
+        self.queue = [0]
+        self.visited = []
+        self.pathRecord = [(0,0)]
+        #self.queue += self.getAdjacencies(env)        
         while (len(self.queue) > 0):
-            parent = self.queue.pop(0);
+            parent = self.queue.pop(0)
             print("Parent = {0}".format(parent))
             if self.squareToNode(env,parent) ==3:
                 print("found path to goal")
+                print("Path record holds:{0}".format(self.pathRecord))
+                self.trimPath(env)
                 return True
             for item in self.expand(env,parent):
                 if item in self.visited:
                     continue
                 if item not in self.queue:
                     self.queue.append(item)
-                    if self.squareToNode(env,item) ==1:
-                        print("about to add an item, taken from expand that is a wall")
+                    self.pathRecord.append((parent,item))
                     print("added Square:{0}: to Queue, Queue is now {1}".format(item,self.queue))
             self.visited.append(parent) 
