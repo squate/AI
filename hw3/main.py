e9ae4d8cf7d2cfaa1e9b69a58fed0709596cb712
squate <tobogganlad@gmail.com>
2018-02-15 17:25:36 -0500

    removed references to deleted data

--- hw3/main.py
+++ hw3/main.py
@@ -3,14 +3,14 @@
 #Nate Levy, Alan Sato, SCI326 hw2
 from envi import *
 from agent import *
-#DEBUG = False
+DEBUG = True
 #:::::logger:::::
-#def log(s):
-#    if DEBUG == True:
-#        print(s)
-#def log2(s,e):
-#    if DEBUG == True:
-#        print(s,end = e)
+def log(s):
+    if DEBUG == True:
+        print(s)
+def log2(s,e):
+    if DEBUG == True:
+        print(s,end = e)
 
 def main():
     dungeon = envi(6)
@@ -20,7 +20,7 @@ def main():
 #    bruce.findHyoo(dungeon,0,0) Testing to see if Heuristic value works
 #    bruce.findHyoo(dungeon,1,0)
 #    bruce.findHyoo(dungeon,0,1)
-    dungeon.showedges()
-#    bruce.BFS(dungeon)
-#    dungeon.showgrid(bruce)
+    bruce.DFS(dungeon)
+    log("final path:{0}".format(bruce.getFinalPath()))
+
 main()
