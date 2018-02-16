5c526bd30472a8895ec46b6949b6be59e95b213c
squate <tobogganlad@gmail.com>
2018-02-15 20:52:45 -0500

    made the test funtion required by lab:

--- hw3/main.py
+++ hw3/main.py
@@ -11,17 +11,39 @@ def log(s):
 def log2(s,e):
     if DEBUG == True:
         print(s,end = e)
+#:::::non-meta functions:::::
 def main():
     dungeon = envi(6)
     bruce = Agent()
-    dungeon.showgrid(bruce)
-#    bruce.findHyoo(dungeon,0,0) Testing to see if Heuristic value works
-#    bruce.findHyoo(dungeon,1,0)
-#    bruce.findHyoo(dungeon,0,1)
-    bruce.BFS(dungeon)
-    log("final path:{0}".format(bruce.getFinalPath()))
-    log("Following:")
-    bruce.followPath(dungeon)
-    log("steps: " + str(bruce.getstepCount()))
-    dungeon.showgrid(bruce)
+    finalTest(bruce, dungeon, 100)
+
+def finalTest(agent,environment,reps):
+    totalBFS = 0
+    totalDFS = 0
+    totalUCS = 0
+    print("testing...")
+    for i in range(reps):
+        dungeon = envi(environment.getSideLength())
+        agent.BFS(environment)
+        agent.followPath(environment)
+        totalBFS += agent.getstepCount()
+    avgBFS = totalBFS / reps
+    print("Avg Steps/ run (BFS): {0}".format(avgBFS))
+
+    for i in range(reps):
+        dungeon = envi(environment.getSideLength())
+        agent.DFS(environment)
+        agent.followPath(environment)
+        totalDFS += agent.getstepCount()
+    avgDFS = totalDFS / reps
+    print("Avg Steps/ run (DFS): {0}".format(avgDFS))
+
+    for i in range(reps):
+        dungeon = envi(environment.getSideLength())
+        agent.UCS(environment)
+        agent.followPath(environment)
+        totalUCS += agent.getstepCount()
+    avgUCS = totalUCS / reps
+    print("Avg Steps/ run (UCS): {0}".format(avgUCS))
+
 main()
