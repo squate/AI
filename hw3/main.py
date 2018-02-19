ee29d320299d613f32c82b728a0226cca9414486
squate <tobogganlad@gmail.com>
2018-02-19 17:43:48 -0500

    fixt

--- hw3/main.py
+++ hw3/main.py
@@ -11,20 +11,21 @@ def log(s):
         print(s)
 #:::::non-meta functions:::::
 def main():
-    dungeon = envi(6)
+    dungeon = envi(6, False)
     bruce = Agent()
     totalGreed = 0
-    finalTest(bruce,dungeon,1000)
+    finalTest(bruce,dungeon,100)
 def finalTest(agent,environment,reps):
     totalBFS = 0
     totalDFS = 0
     totalUCS = 0
     totalGreedy = 0
     totalFail = 0
-
+    totalStepsAStar = 0
+    totalCostAStar = 0
     print("BFS:")
     for i in range(reps):
-        dungeon = envi(environment.getSideLength())
+        dungeon = envi(environment.getSideLength(),False)
         run = agent.BFS(dungeon)
         if run[0] == 1:
             totalBFS += run[1]
@@ -42,7 +43,7 @@ def finalTest(agent,environment,reps):
 
     print("DFS:")
     for i in range(reps):
-        dungeon = envi(environment.getSideLength())
+        dungeon = envi(environment.getSideLength(), False)
         run = agent.DFS(dungeon)
         if run[0] == 1:
             totalDFS += run[1]
@@ -60,7 +61,7 @@ def finalTest(agent,environment,reps):
 
     print("Greedy:")
     for i in range(reps):
-        dungeon = envi(environment.getSideLength())
+        dungeon = envi(environment.getSideLength(), False)
         run = agent.greedy(dungeon)
         if run[0] == 1:
             totalGreedy += run[1]
@@ -79,7 +80,7 @@ def finalTest(agent,environment,reps):
 
     print("UCS:")
     for i in range(reps):
-        dungeon = envi(environment.getSideLength())
+        dungeon = envi(environment.getSideLength(),False)
         run = agent.UCS(dungeon)
         if run[0] == 1:
             totalUCS += run[1]
@@ -93,5 +94,27 @@ def finalTest(agent,environment,reps):
     else:
         print("    Failed every one of its {0} runs".format(reps))
 
+    totalFail = 0
+
+    print("A*:")
+    dungeon = envi(environment.getSideLength(), True)
+    for i in range(reps):
+        dungeon.resetEverythingButCosts()
+        run = agent.aStar(dungeon)
+        if run[0] == 1:
+            totalStepsAStar += run[1]
+            totalCostAStar += run[2]
+        else:
+            totalFail += 1
+            dungeon.showgrid(agent)
+
+    if (totalFail<reps):
+        avg = totalStepsAStar / (reps - totalFail)
+        odds = 100*(reps-totalFail)/reps
+        print("    Avg Steps to exit: {0}".format(avg))
+        print("    Average cost per found path : {0} (would be {1} if not taken into account)".format((totalCostAStar/reps),(21*(environment.getSideLength()-1))))
+        print("    Percent success: {0}\n".format(odds))
+    else:
+        print("    Failed every one of its {0} runs".format(reps))
 
 main()
