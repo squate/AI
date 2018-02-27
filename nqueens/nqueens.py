d92ded1b58cbf5bb6bdb9e2e41943a9323a6ac5c
Alan Sato <halfcreative@gmail.com>
2018-02-27 15:16:42 -0500

    Lots of glitches, need to be more careful nate

--- nqueens/nqueens.py
+++ nqueens/nqueens.py
@@ -7,18 +7,18 @@ DEBUG = True
 
 def log(n):
     if DEBUG == True:
-        print n
+        print(n)
 
 class Env():
     def __init__(self):
         self.size = 4
-        self.queens = self.makeColumns(self)
-        self.totalDanger = self.detectDanger(self)
+        self.queens = self.makeColumns(self.size)
+        self.totalDanger = self.detectDanger()
 
     def makeColumns(self,n):
         col = list()
-        for i in range(self.size-1):
-            col.append((random.randint(0,self.size-1),0))
+        for i in range(n-1):
+            col.append((random.randint(0,n-1),0))
         return col
 
     def detectDanger(self):
@@ -29,18 +29,24 @@ class Env():
                 if self.queens[i] == self.queens[j]:
                     self.noteDanger(i, j)
                 #if same up-right diagonal from queens[i]
-                if self.queens[i] = (self.queens[j] + (j-i)):
+                if self.queens[i] == (self.queens[j] + (j-i)):
                     self.noteDanger(i, j)
                 #same down-right diagonal from queens[i]
-                if self.queens[i] = (self.queens[j] + (i-j)):
+                if self.queens[i] == (self.queens[j] + (i-j)):
                     self.noteDanger(i, j)
 
     def resetDanger(self):
         self.totalDanger = 0
-        for i in range(self.n-1)
+        for i in range(self.size-1):
             self.queens[i][0] = 0
 
     def noteDanger(self, i, j):
         self.totalDanger += 1
         self.queens[i][1] += 1
         self.queens[j][1] += 1
+    def showcol(self):
+        print("{0}".format(self.queens))
+def main():
+    place = Env()
+    place.showcol()
+main()
