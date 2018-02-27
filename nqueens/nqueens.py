c7f96ac80e950c8e67a5c53194050857b0377cbb
Alan Sato <halfcreative@gmail.com>
2018-02-27 16:04:59 -0500

    Display list and grid, Make sure everything is working to setup

--- nqueens/nqueens.py
+++ nqueens/nqueens.py
@@ -10,6 +10,7 @@ def log(n):
         print(n)
 
 class Env():
+
     def __init__(self):
         self.size = 4
         self.queens = self.makeColumns(self.size)
@@ -17,35 +18,50 @@ class Env():
 
     def makeColumns(self,n):
         col = list()
-        for i in range(n-1):
-            col.append((random.randint(0,n-1),0))
+        for i in range(n):
+            col.append([random.randint(0,self.size-1),0])
         return col
 
     def detectDanger(self):
         self.resetDanger()
-        for i in range(self.size - 1):
-            for j in range(i+1, self.size-1):
+        for i in range(self.size):
+            for j in range(i+1, self.size):
                 #if on same row as other
-                if self.queens[i] == self.queens[j]:
+                if self.queens[i][0] == self.queens[j][0]:
                     self.noteDanger(i, j)
                 #if same up-right diagonal from queens[i]
-                if self.queens[i] == (self.queens[j] + (j-i)):
+                if self.queens[i][0] == (self.queens[j][0] + (j-i)):
                     self.noteDanger(i, j)
                 #same down-right diagonal from queens[i]
-                if self.queens[i] == (self.queens[j] + (i-j)):
+                if self.queens[i][0] == (self.queens[j][0] + (i-j)):
                     self.noteDanger(i, j)
 
     def resetDanger(self):
         self.totalDanger = 0
         for i in range(self.size-1):
-            self.queens[i][0] = 0
-
+            self.queens[i][1] = 0
     def noteDanger(self, i, j):
         self.totalDanger += 1
         self.queens[i][1] += 1
         self.queens[j][1] += 1
+
+
+#Display Purposes
     def showcol(self):
+        print("this is what the list looks like (row of queen, number of collisions):")
         print("{0}".format(self.queens))
+        print("this is what the grid would look like based on the list")
+        grid = [[] for i in range(self.size)]
+        for j in range(self.size):
+            for i in range(self.size):
+                if self.queens[i][0] == j : 
+                    grid[j].append("X")
+                else:
+                    grid[j].append("o")
+        for k in range(self.size):
+            print("{}".format(grid[k]))
+
+# Run the Juuls
 def main():
     place = Env()
     place.showcol()
