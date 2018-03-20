0f10718ef4eaa5a313386d62426526c84999d2f2
squate <tobogganlad@gmail.com>
2018-03-20 15:16:51 -0400

    n' moves

--- hw6/sentprob.py
+++ hw6/sentprob.py
@@ -1,55 +1,65 @@
 #! /usr/bin/python3
 # Nate Levy, Alan Sato
-# Sentence Probablility Finder 
-
+# Sentence Probablility Finder
 
 def preprocess(str):
     str = str.lower()
     words = str.split()
-    
-    u = get_unigrams(words)
-    b = get_bigrams(words)
+    words.append("</s>")
 
-    return u, b
-
-def get_unigrams(words):
     print("Creating unigrams...")
     total_uni = 0
     unigrams = {}
-    
+    unigrams["<s>"] = 1
+
     for w in words:
         total_uni += 1
-        if w in unigrams.keys:
+        if w in unigrams:
             unigrams[w] += 1
-        else: 
+        else:
             unigrams[w] = 1
-            
-    #convert from count to probability 
-    for u in unigrams:
-        u.value = u.value/total_uni
-    
-    return unigrams
-    
-def get_bigrams(words):
+    unigrams["</s>"] = 1
+
+    for u in unigrams: #convert from count to probability
+        unigrams[u] = unigrams[u]/total_uni
+
     print("Creating bigrams...")
     total_bi = 0
-    bigram_dump = {}
+    bigs = {}
     prev_word = "<s>"
+
     for w in words:
         total_bi += 1
-        bigram_dump[prev_word].append(w)
+        if (prev_word, w) in bigs: #if this is an identified bigram already
+            bigs[prev_word, w] += 1
+        else:
+            bigs[prev_word, w] = 1
         prev_word = w
-        
-    #convert this bigram dump to a matrix of words, with bigram prob stored
-    w1 = []
-    for i in bigram_dump:
-        bigram_mtx.append[i]
-        
-    return bigram_dump
+    bigs[w, "</s>"] = 1
+
+    for b in bigs:
+        bigs[b] = bigs[b] / total_bi
+        bigs[b] = bigs[b] / unigrams[b[0]]
+
+    return unigrams, bigs
 
 def main():
-    print("Welcome to the Uni-Bigram creaotr!")
-    og_text = input("Enter file name")
+    print("Welcome to the Uni-Bigram creator!")
+    og_text = input("Enter file name: ")
     grams = preprocess(og_text)
-    
+    print("pick an option:\n\t1. Search for unigram\n\t2. Search for bigram\n\t3. Sentence Probablility\n\t4. Exit\n")
+    op = input()
+
+    while op < 4:
+        if op not in range(1,4):
+            print("error! Error! ERROR!1 EEEEEERRRRRRROOOOOORRRRRR!!!!\n\tthat's not an option.")
+        elif op == 1:
+            uni_query = input("Enter a unigram to search")
+            unigram_search(uni_query, grams[0])
+        elif op == 2:
+            bi_query = input("Enter a bigram to search")
+            bigram_search(bi_query, grams[1])
+        elif op == 3:
+            sent_query = input("Enter a sentence to search")
+
 main()
