89519cc12b5577e881e2363a50fbf0efdb23dc04
squate <tobogganlad@gmail.com>
2018-04-03 14:47:04 -0400

    added comments

--- crawl/crawler.py
+++ crawl/crawler.py
@@ -3,8 +3,9 @@
 """
 Created on Wed Mar 28 14:40:24 2018
 
-@author: nate levy, alan sato
+@author: nate levy
 """
+
 import re
 import urllib.request
 from urllib.request import Request
@@ -12,28 +13,34 @@ from http.cookiejar import CookieJar
 
 
 def yoink(url):
-    #get the text of the site
+    #data to give the sites so they let this crawl them
+    cj = CookieJar() #some sites require cookies to access
+    opener = urllib.request.build_opener(urllib.request.HTTPCookieProcessor(cj)) #found this on stackoverflow. it's how you do the cookies.
+    req = Request(url, headers={'User-Agent': 'Mozilla/5.0'}) #spoof a browser
     
-    cj = CookieJar()
-    opener = urllib.request.build_opener(urllib.request.HTTPCookieProcessor(cj))
-    req = Request(url, headers={'User-Agent': 'Mozilla/5.0'})
+    #convert html source to string
     site = opener.open(req).read().decode('utf-8')
-    #make text document
+    
+    #write the string to a new text document
     doc_name = name_doc(url)
     text = open(doc_name, 'w+')
     text.write(site)
-    #find links
+    
+    #find links, format everything to prevent double-yoinking.
     links = []
     maybe = re.findall(r'(?<=href=")([\/[a-z0-9A-Z\.\:-]+)', site)
     
-    #sanitize & doublecheck
+    #fix formatting, append valid sites to links
     for m in maybe:
-       # m = m.group()
         m = m.replace('https://', 'http://')
         m = m.replace('www.', '')
+        # convert incomplete links to complete links
         if m[0] == '/':
             m = url + m
+        #forgive me. there's just some sites that don't work. this was trial and error.
         if 'muhlenberg' in m and 'media' not in m and 'impact' not in m and 'main' not in m and 'twitter' not in m and 'G' not in m:
+            # generalization to exclude non-html. this omits some legit sites, 
+            # like trexler.muhlenberg.edu, but 500 sites is 500 sites.
             if m.count('.') < 2:
                 m = m.rstrip('/')
                 links.append(m)
