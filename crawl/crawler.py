ade2226c640ee66b21cb0509af38512e1b7edb12
squate <tobogganlad@gmail.com>
2018-04-03 11:17:51 -0400

    made it work 112%

--- crawl/crawler.py
+++ crawl/crawler.py
@@ -3,7 +3,7 @@
 """
 Created on Wed Mar 28 14:40:24 2018
 
-@author: nate
+@author: nate levy, alan sato
 """
 import re
 import urllib.request
@@ -13,29 +13,30 @@ from http.cookiejar import CookieJar
 
 def yoink(url):
     #get the text of the site
+    
     cj = CookieJar()
     opener = urllib.request.build_opener(urllib.request.HTTPCookieProcessor(cj))
     req = Request(url, headers={'User-Agent': 'Mozilla/5.0'})
-    site = opener.open(req).read().decode('utf8')
+    site = opener.open(req).read().decode('utf-8')
     #make text document
     doc_name = name_doc(url)
     text = open(doc_name, 'w+')
     text.write(site)
     #find links
     links = []
-    
     maybe = re.findall(r'(?<=href=")([\/[a-z0-9A-Z\.\:-]+)', site)
-    #print(maybe)
+    
+    #sanitize & doublecheck
     for m in maybe:
        # m = m.group()
         m = m.replace('https://', 'http://')
         m = m.replace('www.', '')
-
         if m[0] == '/':
             m = url + m
-        if 'muhlenberg' in m and '.css' not in m and '.ico' not in m and 'media' not in m and '.php' not in m and '.co1' not in m:
-            m = m.rstrip('/')
-            links.append(m)
+        if 'muhlenberg' in m and 'media' not in m and 'impact' not in m and 'main' not in m and 'twitter' not in m and 'G' not in m:
+            if m.count('.') < 2:
+                m = m.rstrip('/')
+                links.append(m)
 
     return links
 
@@ -53,7 +54,7 @@ def main():
     next = [start_url]
     past_links = []
     count = 0
-    while len(next) !=0 and count < 500:
+    while len(next) !=0 and count <= 500:
         next_url = next.pop(0)
         if next_url not in past_links:
             print('accessing '+next_url + '\t{0}'.format(count))
